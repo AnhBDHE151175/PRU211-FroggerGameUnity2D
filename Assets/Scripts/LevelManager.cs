2db216ad720978a4becd44f0fa8e8d605e7b9f9c
Nghiafpt178 <nghianxfpt153281@gmail.com>
2022-10-24 22:38:07 +0700

    Update project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void loadLevel1()
+    {
+        Application.LoadLevel("Level1");
+    }
+    
+
+}
