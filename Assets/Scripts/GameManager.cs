2a025c5d473f2b4e470abe0e98fc3bae8ff353f6
Nghiafpt178 <nghianxfpt153281@gmail.com>
2022-10-24 18:49:59 +0700

    Add game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void NewGame()
+    {
+
+    }
+
+    private void NewLevel()
+    {
+
+    }
+
+    private void NewRound()
+    {
+
+    }
+}
