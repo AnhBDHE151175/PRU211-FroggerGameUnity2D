2d28842c64fe9148f8b3bd6c06af808ddb33921b
Nghiafpt178 <nghianxfpt153281@gmail.com>
2022-10-30 15:02:53 +0700

    Add score,lives,timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,80 +1,159 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private Home[] homes; 
+    private Frogger frogger;
+    private Home[] homes;
+
+    public GameObject gameOverMenu;
+    public Text timeText;
+    public Text livesText;
+    public Text scoreText;
+
     private int lives;
-
-    private int time;
     private int score;
+    private int time;
 
+    private float respawnTime;
 
-    Frogger frogger;
     private void Awake()
     {
-        homes = FindObjectsOfType<Home>();  
+        Application.targetFrameRate = 60;
+
+        homes = FindObjectsOfType<Home>();
         frogger = FindObjectOfType<Frogger>();
     }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
     private void NewGame()
     {
+        gameOverMenu.SetActive(false);
+
         SetScore(0);
         SetLives(3);
         NewLevel();
     }
+
     private void NewLevel()
     {
-        for(int i = 0; i< homes.Length; i++)
-        {
+        for (int i = 0; i < homes.Length; i++) {
             homes[i].enabled = false;
         }
-        NewRound();
-        
+
+        Respawn();
     }
 
-    private void NewRound()
+    private void Respawn()
     {
         frogger.Respawn();
+
+        StopAllCoroutines();
+        StartCoroutine(Timer(30));
     }
+
+    private IEnumerator Timer(int duration)
+    {
+        time = duration;
+        timeText.text = time.ToString();
+
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1);
+
+            time--;
+            timeText.text = time.ToString();
+        }
+
+        frogger.Death();
+    }
+
+    public void Died()
+    {
+        SetLives(lives - 1);
+
+        if (lives > 0) {
+            Invoke(nameof(Respawn), 1f);
+        } else {
+            Invoke(nameof(GameOver), 1f);
+        }
+    }
+
+    private void GameOver()
+    {
+        frogger.gameObject.SetActive(false);
+        gameOverMenu.SetActive(true);
+
+        StopAllCoroutines();
+        StartCoroutine(CheckForPlayAgain());
+    }
+
+    private IEnumerator CheckForPlayAgain()
+    {
+        bool playAgain = false;
+
+        while (!playAgain)
+        {
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                playAgain = true;
+            }
+
+            yield return null;
+        }
+
+        NewGame();
+    }
+
+    public void AdvancedRow()
+    {
+        SetScore(score + 10);
+    }
+
     public void HomeOccupied()
     {
         frogger.gameObject.SetActive(false);
+
+        int bonusPoints = time * 20;
+        SetScore(score + bonusPoints + 50);
+
         if (Cleared())
         {
+            SetLives(lives + 1);
+            SetScore(score + 1000);
             Invoke(nameof(NewLevel), 1f);
         }
-        else
-        {
-            Invoke(nameof(NewRound), 1f);
+        else {
+            Invoke(nameof(Respawn), 1f);
         }
     }
 
-    public bool Cleared()
+    private bool Cleared()
     {
         for (int i = 0; i < homes.Length; i++)
         {
-            if (!homes[i].enabled)
-            {
+            if (!homes[i].enabled) {
                 return false;
-            }   
+            }
         }
+
         return true;
     }
+
     private void SetScore(int score)
     {
         this.score = score;
+        scoreText.text = score.ToString();
     }
 
     private void SetLives(int lives)
     {
         this.lives = lives;
+        livesText.text = lives.ToString();
     }
 
-    private void GameOver()
-    {
-
-    }
 }
-  
--- Assets/Scripts/Home.cs
+++ Assets/Scripts/Home.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 public class Home : MonoBehaviour
 {
     public GameObject frog;
- 
+    private BoxCollider2D boxCollider;
 
-   
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void OnEnable()
     {
         frog.SetActive(true);
-      
+        boxCollider.enabled = false;
+
     }
 
     private void OnDisable()
     {
         frog.SetActive(false);
-       
+        boxCollider.enabled = true;
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,7 +30,7 @@ public class Home : MonoBehaviour
         if (!enabled && other.gameObject.CompareTag("Player"))
         {
             enabled = true;
-            FindObjectOfType<Frogger>().Respawn();
+            FindObjectOfType<GameManager>().HomeOccupied();
         }
     }
 
